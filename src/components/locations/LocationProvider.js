0930fe24eec52b2ee1b07711f87dc7336293021d
Xine75 <cjdoza@gmail.com>
2021-01-25 14:19:21 -0600

    initial

--- src/components/locations/LocationProvider.js
+++ src/components/locations/LocationProvider.js
@@ -8,13 +8,13 @@ export const LocationProvider = (props) => {
     const [locations, setLocations] = useState([])
 
     const getLocations = () => {
-        return fetch("http://localhost:8088/locations?_expand=location")
+        return fetch("http://localhost:8080/locations?_expand=location")
         .then(res => res.json())
         .then(setLocations)
     }
 
     const addLocation = locationObj => {
-        return fetch("http://localhost:8088/locations", {
+        return fetch("http://localhost:8080/locations", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
