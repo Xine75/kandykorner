6ee46cd308f611c40643fcc02a5d09a03a1d1e50
Xine75 <cjdoza@gmail.com>
2021-01-26 19:32:53 -0600

    login

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
+
 import { LocationProvider } from "./locations/LocationProvider"
 import { LocationList } from "./locations/LocationList"
+
 import { ProductProvider } from "./products/ProductProvider"
 import { ProductList } from "./products/ProductList"
+
 import { EmployeeProvider } from "./employees/EmployeeProvider"
 import { EmployeeList } from "./employees/EmployeeList"
 import { EmployeeForm } from "./employees/EmployeeForm"
